94e2431e33305cb3f3784a4308117d3fdfaf756a
mikeHerceg <mikehercegdesigns@gmail.com>
2022-01-10 10:07:54 -0500

    creating store with react context

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import "./main.scss";
-import React, { lazy, Suspense } from 'react';
+import React, { lazy, Suspense , createContext } from 'react';
 import {
   HashRouter as Router,
   Switch,
@@ -7,23 +7,28 @@ import {
 } from "react-router-dom";
 import Navigation from "./components/navigation/navigation";
 
+
 const Home = lazy(() => import('./views/home'));
 const MyDrinks = lazy(() => import('./views/my-drinks'));
 
+export const Store = createContext();
+
+const initialstate = {
+};
+
 export const App = () =>{
   return(
-    <>
-   
-    <Router>   
-      <Navigation/>
-      <Suspense fallback={"loading..."}>
-        <Switch>
-            <Route path={"/my-drinks"} component={MyDrinks}/>
-            <Route path={"/"} component={Home}/>
-        </Switch>   
-      </Suspense>
-    </Router>
-    </>
+    <Store.Provider value={initialstate}>
+      <Router>   
+        <Navigation/>
+        <Suspense fallback={"loading..."}>
+          <Switch>
+              <Route path={"/my-drinks"} component={MyDrinks}/>
+              <Route path={"/"} component={Home}/>
+          </Switch>   
+        </Suspense>
+      </Router>
+    </Store.Provider>
   );
 };
 
